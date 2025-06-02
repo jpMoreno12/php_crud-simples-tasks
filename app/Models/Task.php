f7411a95749f221414ccb3a42bd4721450fc55b2
morendev <morendev@MorenD.morendev>
2025-06-01 20:32:22 -0400

    feat: cria relacionamento um pra muitos entre tabela users e tabela tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-   protected $fillable = ['user_id', 'title', 'description', 'status'];
-   
+    protected $fillable = ['user_id', 'title', 'description', 'status'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Dom\Comment;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +47,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function tasks() : HasMany
+    {
+        return $this->hasMany(Task::class, 'user_id', 'id')->chaperone();
     }
 }
