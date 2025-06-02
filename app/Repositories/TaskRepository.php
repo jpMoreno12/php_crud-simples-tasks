761e25f384ce26c911190f600409029c180fd8ca
morendev <morendev@MorenD.morendev>
2025-06-02 01:51:57 -0400

    implementa arquitetura services e repositories

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+use App\Models\Task;
+use App\Repositories\Contracts\ITask;
+class TaskRepository implements ITask {
+    public Task $task;  
+
+    public function __construct(Task $task) {
+        $this->task = $task;
+    }
+
+    public function createTask(array $task) {
+        return $this->task->create($task);
+    }
+    public function getAllTasks(int $user_id){
+        return $this->task->where('user_id', $user_id)->get();
+    }
+    public function getTask(int $id){
+        return $this->task->find($id);
+    }
+    public function updateTask(int $id, array $data){ 
+        $task = $this->task->find($id);
+
+        if($task) {
+            $task->update($data);
+            return $task;
+        }
+        return false;
+    }
+    public function deleteTask(int $id){
+        $task = $this->task->find($id);
+
+        if($task) {
+            return $task->delete();
+        }
+        return false;
+    }
+
+}
