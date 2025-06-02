1640920afeb6892bf9bc4f8ca5b830c1b0495ccc
morendev <morenodasilvajoao709@gmail.com>
2025-06-02 15:36:22 -0300

    feat: adiciona middleware AddCustomHeader

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->use([
+            \App\Http\Middleware\AddCustomHeader::class,    
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
+use App\Http\Middleware\AddCustomHeader;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -28,6 +29,9 @@ Route::post('/login', [UserController::class, 'login']);
 Route::get('/getAllUsers', [UserController::class, 'getAllUsers']);      
 Route::get('/getUser{id}', [UserController::class, 'getUser']);     
 Route::post('/createUser', [UserController::class, 'createUser']);     
+
+
+Route::get('teste', [UserController::class, 'teste']);
 // Route::put('/updateUser', [UserController::class, 'updateUser']);  
 // Route::delete('/deleteUser', [UserController::class, 'deleteUser']); 
 
